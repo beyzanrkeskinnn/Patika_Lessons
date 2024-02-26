602aee6114f4367b79e6420ade9a255648e7af51
Beyzanur KESKİN <beyzanrkeskinn94@gmail.com>
2024-02-26 18:54:33 +0300

    Added method according to pattern

--- Week_2/src/MethodByPattern.java
+++ Week_2/src/MethodByPattern.java
@@ -1,25 +1,32 @@
 import java.util.Scanner;
 
 public class MethodByPattern {
-    static int pattern(int number) {
-        int result=1;
+    static int pattern(int number, boolean control, int counter) {  // Metot pattern
 
-if(number>0){
-    result = number - 5;
-    System.out.println(result);
-    if (number <= 0) {
-        result = number + 5;
-        System.out.println(result);
-    }
-}
-      return number ;
-
+        // control ile girilen sayı kontrol edilir.
+        if (control) {
+            if (number > 0) { // number >0 olacağı için değeri true'dur.
+                System.out.print(number + " ");
+                counter++;
+                return pattern(number - 5, true, counter);
+            } else {
+                return pattern(number, false, counter);
+            }
+        } else {
+            System.out.print(number + " ");
+            if (counter > 0) {
+                counter--;
+                return pattern(number + 5, false, counter);
+            } else {
+                return 0;
+            }
+        }
     }
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Bir sayı giriniz: ");
-        int number = scan.nextInt();
-pattern(number);
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı giriniz: ");
+        int number = input.nextInt();
+        System.out.println(pattern(number, true, 0));   //counter sayac değişkeni, 0'dan başlamalı
     }
 }
