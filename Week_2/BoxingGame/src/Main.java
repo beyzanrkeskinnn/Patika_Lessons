5784620cd0c88fa55bf37e18f541fc5e159ff0df
Beyzanur KESKİN <beyzanrkeskinn94@gmail.com>
2024-02-28 23:10:50 +0300

    Boxing Game Added

---
+++ Week_2/BoxingGame/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Fighter f1 = new Fighter("A", 10, 120, 100, 30);
+        Fighter f2 = new Fighter("B", 20, 85, 85, 40);
+
+        Match match = new Match(f1, f2, 80, 100);
+        match.run();
+
+    }
+}
