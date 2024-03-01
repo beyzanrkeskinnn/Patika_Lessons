5b9ecadaf3b6b00921dfb231e869003ef15c61bf
Beyzanur KESKİN <beyzanrkeskinn94@gmail.com>
2024-03-01 22:07:44 +0300

    Salary calculation added2

---
+++ Week_2/SalaryCalculator/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Employee personel1 = new Employee("beyza", 5000, 50, 1998);
+        personel1.tax();
+        personel1.bonus();
+        personel1.raiseSalary();
+        personel1.personelToString();
+        }
+    }
