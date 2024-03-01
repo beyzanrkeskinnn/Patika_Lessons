e0817792b71d76b3afc2113a7ba5185dd41c2bd5
Beyzanur KESKİN <beyzanrkeskinn94@gmail.com>
2024-03-01 22:06:05 +0300

    Salary calculation added

---
+++ Week_2/SalaryCalculator/src/Employee.java
@@ -0,0 +1,54 @@
+public class Employee {
+    String name;
+    double salary;
+    int workHours;
+    int hireYear;
+    double tax;
+    double bonus;
+    double raise;
+
+    Employee(String name, double salary, int workHours, int  hireYear){
+    this.name=name;
+    this.hireYear=hireYear;
+    this.salary=salary;
+    this.workHours=workHours;
+
+    }
+     void tax (){
+        if(this.salary>0 && this.salary<1000){
+            this.tax =this.salary*0;
+        }else{
+            this.tax=this.salary*0.3;
+
+        }
+     }
+     void bonus() {
+         if (this.workHours > 40) {
+             this.bonus = 30 * (this.workHours - 40);
+
+         }
+     }
+        void raiseSalary(){
+            if(2021-hireYear<10){
+                this.raise=this.salary*0.5;
+            }
+            else if(2021-hireYear>9 && 2021-hireYear<20){
+                this.raise=this.salary*0.10;
+            }
+            else if(2021-hireYear>19){
+                this.raise=this.salary*0.15;
+            }
+         }
+         void personelToString(){
+             System.out.println("Adı : " + this.name);
+             System.out.println("Maaşı : " + this.salary);
+             System.out.println("Çalışma Saati : " + this.workHours);
+             System.out.println("Başlangıç Yılı : " + this.hireYear);
+             System.out.println("Vergi : " + this.tax);
+             System.out.println("Bonus : " + this.bonus);
+             System.out.println("Maaş Artışı : " + this.raise);
+             System.out.println("Vergi ve Bonuslar ile birlikte maaş : " + (this.salary + this.bonus - this.tax));
+             System.out.println("Toplam maaş : " + ((this.salary + this.bonus - this.tax) + this.raise));
+         }
+     }
+
