1017e27d78dceaf36d4160a0e6c93a6c0562e1b0
Beyzanur KESKİN <beyzanrkeskinn94@gmail.com>
2024-04-09 14:48:29 +0300

    Added Patika Store Week_6

--- Week_6/src/Main.java
+++ Week_6/src/Main.java
@@ -1,32 +1,24 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) {
         String path = "D:\\Java\\Patika_Lessons\\Week_6\\src/sayilar.txt";
         int toplam = 0;
-        try {
-            // Dosyayı okumak için Scanner kullanıyoruz
-            File dosya = new File(path);
-            Scanner scanner = new Scanner(dosya);
 
-            // Dosyadaki her bir satırı okuyarak sayıları toplam değişkenine ekliyoruz
-            while (scanner.hasNextLine()) {
-                int sayi = Integer.parseInt(scanner.nextLine());
-                toplam += sayi;
+        // Dosyayı okumak için Scanner kullanıyoruz
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String satir; // Dosyadan okunan satırı tutmak için değişken
+            while ((satir = br.readLine()) != null) {
+                int sayi = Integer.parseInt(satir); // Satırı tam sayıya dönüştürme
+                toplam += sayi; // Toplama ekleme
             }
-
-            // Sonucu ekrana yazdırıyoruz
-            System.out.println("Dosyadaki sayıların toplamı: " + toplam);
-
-            // Scanner'ı kapatıyoruz
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("Dosya bulunamadı: " + e.getMessage());
+            System.out.println("Dosyadaki sayıların toplamı: " + toplam); // Toplamı ekrana yazdırma
+        } catch (IOException e) {
+            System.err.println("Dosya okuma hatası: " + e.getMessage());
         }
+
+
     }
 }
 
